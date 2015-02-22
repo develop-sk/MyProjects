cadaea23bd67e05f2269524c0e40973b73b13059
Net Developer <netdeveloper@learning.com>
2015-02-22 18:45:36 -0500

    Created empty NotePad-- Window

---
+++ WPF/Note_Pad--/Note_Pad--/ApplicationWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+
+namespace Note_Pad__
+{
+    class ApplicationWindow : Window
+    {
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            MainWindow win = new MainWindow();
+            app.Run(win);
+        }
+    }
+}
---
+++ WPF/Note_Pad--/Note_Pad--/MainWIndow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Note_Pad__
+{
+    class MainWindow : Window
+    {
+        private Grid gridPanel;
+        
+        public MainWindow()
+        {
+            Title = "NotePad--";
+            Height = 400;
+            Width = 500;
+
+            gridPanel = new Grid();
+            Content = gridPanel;
+
+           
+
+        }
+    }
+}
